9d2e9f9c08d3ab85868f10eb6536b6f5744b133c
MansourM61 <22415290+MansourM61@users.noreply.github.com>
2026-03-26 23:09:14 +0000

    App is turned into a SPA (no more MPA).

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,56 @@
-import { fileURLToPath, resolve, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 import TurboConsole from 'unplugin-turbo-console/vite'
+import { defineConfig, type UserConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
 
-export default {
-    root: process.cwd(),
-    base: '/',
-    publicDir: 'public',
-    envDir: process.cwd(),
-    devtools: true,
-    plugins: [
-        Inspect(),
-        TurboConsole({
-            highlight: {
-                themeDetect: true,
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+    // common configuration shared all environment
+    const sharedConfig = {
+        root: process.cwd(),
+        base: '/',
+        publicDir: 'public',
+        envDir: process.cwd(),
+        devtools: true,
+        plugins: [
+            Inspect(),
+            TurboConsole({
+                highlight: {
+                    themeDetect: true,
+                },
+                inspector: {
+                    printUrl: false,
+                },
+            }),
+            tailwindcss(),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+                '@assets': fileURLToPath(
+                    new URL('./src/assets', import.meta.url)
+                ),
             },
-            inspector: {
-                printUrl: false,
-            },
-        }),
-        tailwindcss(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-            '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
         },
-    },
-}
+        appType: 'spa', // all requests to all routes will be directed to "index.html".
+        build: {
+            outDir: 'dist',
+        },
+        preview: {
+            allowedHosts: true, // to allow all incoming request from all addresses
+        },
+    } satisfies UserConfig
+
+    if (command === 'serve') {
+        return {
+            // dev specific config
+            ...sharedConfig,
+        }
+    } else {
+        // command === 'build'
+        return {
+            // build specific config
+            ...sharedConfig,
+        }
+    }
+})
